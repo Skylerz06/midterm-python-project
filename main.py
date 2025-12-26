d2447d33bf7e918eb9ab404cdca93b92b1ed142c
nAehT157 <24110157@student.hcmute.edu.vn>
2025-12-26 07:53:22 +0700

    Replace bugfix_ratio with commit_type_ratio

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # main.py
 from src.preprocess import load_and_clean_data
-from src.analysis import commits_per_month, top_authors, bugfix_ratio
+from src.analysis import commits_per_month, top_authors, commit_type_ratio
 from src.visualization import (
     plot_commits_per_month,
     plot_top_authors,
-    plot_bugfix_ratio
+    plot_commit_type_ratio,
 )
 
 DATA_PATH = './data/commit_20000.csv'
@@ -14,11 +14,12 @@ def main():
 
     commits_month = commits_per_month(df)
     authors = top_authors(df)
-    bug_ratio = bugfix_ratio(df)
+    commit_types = commit_type_ratio(df)
 
     plot_commits_per_month(commits_month)
     plot_top_authors(authors)
-    plot_bugfix_ratio(bug_ratio)
+    commit_types = commit_type_ratio(df)
+    plot_commit_type_ratio(commit_types)
 
 if __name__ == '__main__':
     main()
